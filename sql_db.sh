4db05dbc2d5f8fb11d3bf74a44603fdde5f8f621
igorkhod <higo@inbox.ru>
2025-08-26 09:54:32 +0700

    25.08.2025 9:41 sql schedule.sql 9

--- sql_db.sh
+++ sql_db.sh
@@ -2,4 +2,4 @@
 # Краткая аннотация: запускает SQL-проверку расписания CRM-базы
 # Использует check_schedule.sql и базу /var/data/crm.db
 
-sqlite3 /var/data/crm.db < check_schedule.sql
+sqlite3 /var/data/crm.db < schedule.sql
